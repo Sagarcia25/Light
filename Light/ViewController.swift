3e88f96de4ba05ebe472ec2f50318fafbe612498
Salvador Garcia <salvadorgarcia@salvadors-macbook-pro.local>
2019-01-26 11:37:45 -0600

    Deleted text, changed the if statement to a ternary, expanded the button to cover entire screen

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
 var lightOn = true
     
-    @IBOutlet weak var lightButton: UIButton!
+    
     
     @IBAction func buttonPressed(_ sender: UIButton) {
         lightOn = !lightOn
@@ -20,17 +20,11 @@ var lightOn = true
     }
     
     func updateUI(){
-        if lightOn {
-            view.backgroundColor = .white
-            lightButton.setTitle("Off", for: .normal)
-        } else {
-            view.backgroundColor = .black
-            lightButton.setTitle("On", for : .normal)
-        }
+    view.backgroundColor = lightOn ? .white : .black
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        updateUI()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
