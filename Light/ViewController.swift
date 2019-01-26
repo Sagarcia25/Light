a8412bc7e6cfb8dd435cc4e94ee4a7617806fab6
Salvador Garcia <salvadorgarcia@salvadors-macbook-pro.local>
2019-01-26 11:10:40 -0600

    Added the change of text from off to on when button is pressed

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
 var lightOn = true
     
+    @IBOutlet weak var lightButton: UIButton!
     
     @IBAction func buttonPressed(_ sender: UIButton) {
         lightOn = !lightOn
@@ -21,12 +22,15 @@ var lightOn = true
     func updateUI(){
         if lightOn {
             view.backgroundColor = .white
+            lightButton.setTitle("Off", for: .normal)
         } else {
             view.backgroundColor = .black
+            lightButton.setTitle("On", for : .normal)
         }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
