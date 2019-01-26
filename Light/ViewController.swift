cf83112542bb2a5ede5f046aebda772007c3f77c
Salvador Garcia <salvadorgarcia@salvadors-macbook-pro.local>
2019-01-26 10:59:10 -0600

    Made if statement for background view and added a button

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+var lightOn = true
+    
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        lightOn = !lightOn
+        updateUI()
+    }
+    
+    func updateUI(){
+        if lightOn {
+            view.backgroundColor = .white
+        } else {
+            view.backgroundColor = .black
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
